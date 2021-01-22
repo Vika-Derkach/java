dc3755e18f7e4c24379e2b7130cbb62e232ed82d
Vika <queenviktoriyphpgmail.com>
2021-01-22 19:32:07 +0200

    first hm in java

---
+++ Java/src/js/script.js
@@ -0,0 +1,36 @@
+var answer = +prompt("Ваш бюджет?");
+var answer = +prompt("Название вашего магазина?");
+
+var mainList = {
+    budget: 100000,
+    shopName: "Merryland",
+    open: true
+    
+}
+
+console.log(mainList);
+
+
+var employers = {
+    name: "Kata",
+    age: 91,
+    name: "Fina",
+    age: 35,
+    name: "George",
+    age: 47
+    
+}
+
+console.log(employers);
+
+var  shopGoods = ['shoes', 'shirts', 'sweaters', 'trousers'];
+alert (shopGoods [1]);
+
+var typeShopOne = prompt("Какой тип товаров будем продавать?" , "shoes");
+var typeShopTwo = prompt("Какой тип товаров будем продавать?" , " shirts");
+var typeShopThree = prompt("Какой тип товаров будем продавать?" , " sweaters");
+alert("100hr per day");
+
+var number = 33721;
+console.log(3*3*7*2*1);
+console.log(126 ** 3);
