40ae9eef046212a13bb9a26db4532a47163d80b7
Vika <queenviktoriyphpgmail.com>
2021-01-23 18:56:15 +0200

    function sttart lesson 3

---
+++ Java/functions-les_3/js/js.js
@@ -0,0 +1,61 @@
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     alert(text);
+//     num = 10;
+// }
+
+// showFirstMessage("Hello World!!!")
+
+// console.log(num);
+
+
+//буде рахувати все : те що перед функцією і після
+
+// console.log(calc(4,5));
+// console.log(calc(55,676));
+// console.log(calc(456,44));
+// console.log(calc(66,7));
+
+// function calc(a,b) {
+//     return a+b
+// }
+
+
+
+// рахує ТІЛЬКИ після функції function expression
+
+// let calc = function (a,b) {
+//     return
+// }
+
+// console.log(calc(456,44));
+// console.log(calc(66,7));
+
+
+// современий метод
+
+// let calc = (a,b) => a+b
+
+// console.log(calc(456,44));
+// console.log(calc(66,7));
+
+
+// let str = "Test";
+
+// console.log(str.toUpperCase()); //всі букви великі робить 
+// console.log(str.toLowerCase()); // всі букви великі робить
+
+
+// заокруглення
+let str = "12.2px"
+
+// console.log(Math.round(str));
+
+//заберает строку 
+//не читает точек
+console.log(parseInt(str));
+//читает точки
+console.log(parseFloat(str));
+
+
