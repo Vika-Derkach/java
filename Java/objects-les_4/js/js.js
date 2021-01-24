c84e2acd37d80a927a502412caa9ed05f6e066b4
Vika <queenviktoriyphpgmail.com>
2021-01-24 12:40:27 +0200

    lesson 4 масиви

---
+++ Java/objects-les_4/js/js.js
@@ -0,0 +1,51 @@
+// let options = {
+//     name: "test",
+//     width: 1024,
+//     height: 1024
+// }
+
+// options.bool = false;
+// options.colors = {
+//     border: "black",
+//     background: "red"
+// }
+
+// delete options.bool
+
+// for( let key in options) {
+//     console.log('свойство' + key + "имеет значения " + options[key])
+// }
+
+// console.log(Object.keys(options).length);
+// //console.log(options)
+
+/// масиви 
+let arr = [1,2,3,4,5]
+
+// удаления последниво елемента
+//arr.pop();
+
+//виведення в консоль того елемента над яким проводиться дія (видалення)
+//console.log(arr.pop());
+
+// додавання елементу в масив
+//arr.push(6);
+
+//заберає перший елемент
+//arr.shift();
+
+//добавляє
+//arr.unshift(0);
+
+
+//вивести на екран числа
+// for (let i = 0; i <arr.length; i++) {
+//     alert(arr[i]);
+// }
+
+arr[99] = 99;
+console.log(arr.length);
+
+
+
+//console.log(arr);
