b0db5e255f6332730cfb7480558befec7fc94f77
Vika <queenviktoriyphpgmail.com>
2021-01-28 15:37:51 +0200

    lesson8 пареметри и отступи, зробила таймер

--- Java/site/js/script.js
+++ Java/site/js/script.js
@@ -34,4 +34,71 @@ window.addEventListener('DOMContentLoaded', function(){
            }
        };
     });
+
+    //Timer
+    let deadline = '2021-01-29';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor ( (t/1000) % 60),
+        minutes = Math.floor ((t/1000/60) % 60),
+        hours = Math.floor ((t/(1000*60*60)));
+
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+       // if 
+    };
+
+    function setClock(id, endtime) {
+       
+         let timer = document.getElementById(id),
+             hours = timer.querySelector('.hours'),
+             minutes = timer.querySelector('.minutes'),
+             seconds = timer.querySelector('.seconds');
+
+            function updateClock() {
+                let t = getTimeRemaining(endtime);
+                hours.innerHTML = t.hours;
+                minutes.innerHTML = t.minutes;
+                seconds.innerHTML = t.seconds;
+
+                if (t.total <= 0) {
+                  clearInterval(timeInterval);
+                }
+            };
+
+            updateClock();
+             
+            let timeInterval = setInterval(updateClock, 1000);
+
+            
+    };
+
+    setClock('timer', deadline);
+
+
+///плавная прокрутка
+
+    const anchors = document.querySelectorAll('a.scroll-to')
+
+     for (let anchor of anchors) {
+       anchor.addEventListener('click', function (e) {
+         e.preventDefault()
+         
+         const blockID = anchor.getAttribute('href')
+         
+         document.querySelector(blockID).scrollIntoView({
+           behavior: 'smooth',
+           block: 'start'
+         })
+       })
+     }
+
+
+
 });
