464d20cb8342d4a7b35edd952257fe8ec1a9f70a
Vika <queenviktoriyphpgmail.com>
2021-01-29 12:20:41 +0200

    добавила модальне в табах

--- Java/site/js/script.js
+++ Java/site/js/script.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', function(){
     });
 
     //Timer
-    let deadline = '2021-01-29';
+    let deadline = '2021-01-30';
     
     function toZero(time) {
         if (time < 0 ) {
@@ -114,13 +114,45 @@ window.addEventListener('DOMContentLoaded', function(){
      }
 
 
-    //Modal
+    //Modal timer
 
     let more = document.querySelector('.more'),
         overlay = document.querySelector('.overlay'),
-        close = document.querySelector('.popup-close');
+        close = document.querySelector('.popup-close'),
+        description_1 = document.getElementsByClassName('description-btn')[0],
+        description_2 = document.getElementsByClassName('description-btn')[1];
+        description_3 = document.getElementsByClassName('description-btn')[2];
+        description_4 = document.getElementsByClassName('description-btn')[3];
 
     more.addEventListener('click', function() {
+        this.classList.add('more-splash');
+        overlay.style.display = "block";
+        document.body.style.overflow = 'hidden';
+
+    });
+
+    description_1.addEventListener('click', function() {
+        this.classList.add('more-splash');
+        overlay.style.display = "block";
+        document.body.style.overflow = 'hidden';
+
+    });
+
+    description_2.addEventListener('click', function() {
+        this.classList.add('more-splash');
+        overlay.style.display = "block";
+        document.body.style.overflow = 'hidden';
+
+    });
+
+    description_3.addEventListener('click', function() {
+        this.classList.add('more-splash');
+        overlay.style.display = "block";
+        document.body.style.overflow = 'hidden';
+
+    });
+
+    description_4.addEventListener('click', function() {
         this.classList.add('more-splash');
         overlay.style.display = "block";
         document.body.style.overflow = 'hidden';
@@ -132,5 +164,9 @@ window.addEventListener('DOMContentLoaded', function(){
         more.classList.remove('more-splash');
         document.body.style.overflow = '';
     });
+
+
+
+    
 
 });
