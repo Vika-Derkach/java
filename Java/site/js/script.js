bfe22ecc3c62f7c4146bc8c29b23b1866e6a2c6b
Vika <queenviktoriyphpgmail.com>
2021-01-31 20:23:19 +0200

    добавила в форму фітбек

--- Java/site/js/script.js
+++ Java/site/js/script.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function(){
     });
 
     //Timer
-    let deadline = '2021-01-30';
+    let deadline = '2021-02-03';
     
     function toZero(time) {
         if (time < 0 ) {
@@ -191,7 +191,154 @@ window.addEventListener('DOMContentLoaded', function(){
     // }
     
     // splash.addEventListener('click', myAnimation);
+   
 
+    //form
+    let message = new Object();
+    message.loading = "Загрузка...."
+    message.success = "дякую, скоро з вами звяжимося";
+    message.failure = " что то пошло не так";
+
+    let form = document.getElementsByClassName('main-form')[0],
+        ourForm = document.getElementById('form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+        statusMessage.classList.add('status');
+        
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+
+            //AJAX
+            let request = new XMLHttpRequest();
+            request.open("GET", 'https://swapi.dev/api/people/');
+
+            request.setRequestHeader("Content-Type", "application/json");
+
+            // let formData = new FormData(form);
+
+            request.send();
+
+            request.onreadystatechange = function() {
+                if (request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+
+                } else if (request.readyState === 4) {
+                    if (request.status == 200 && request.status < 300) {
+                        console.log({res: JSON.parse(request.response)});
+                        statusMessage.innerHTML = message.success;
+                        // добавіть Kонтент на страницу
+                    }
+                    else {
+                        statusMessage.innerHTML = message.failure;
+                        console.log({request});
+                    }
+                }
+            }
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+                //очищаем поля ввода
+            }
+
+
+
+        });
+
+        //contacts
+        
+        ourForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            ourForm.appendChild(statusMessage);
+
+            //AJAX
+            let request = new XMLHttpRequest();
+            request.open("GET", 'https://swapi.dev/api/people/');
+
+            request.setRequestHeader("Content-Type", "application/json");
+
+            // let formData = new FormData(form);
+
+            request.send();
+
+            request.onreadystatechange = function() {
+                if (request.readyState < 4) {
+                    statusMessage.innerHTML = message.loading;
+
+                } else if (request.readyState === 4) {
+                    if (request.status == 200 && request.status < 300) {
+                        console.log({res: JSON.parse(request.response)});
+                        statusMessage.innerHTML = message.success;
+                        // добавіть Kонтент на страницу
+                    }
+                    else {
+                        statusMessage.innerHTML = message.failure;
+                        console.log({request});
+                    }
+                }
+            }
+            for (let i = 0; i < input.length; i++) {
+                input[i].value = '';
+                //очищаем поля ввода
+            }
+
+
+
+        });
+  
+
+
+   
+
+        // //contacts
+        // let message = new Object();
+        // message.loading = "Загрузка, чекайте ще...."
+        // message.success = "дякую, скоро з вами звяжимося)";
+        // message.failure = " что то пошло не так";
+    
+        // let ourForm = document.getElementsByClassName('form')[0],
+        //     input = form.getElementsByTagName('input'),
+        //     statusMessage = document.createElement('div');
+        //     statusMessage.classList.add('status');
+    
+        //     ourForm.addEventListener('submit', function(event) {
+        //         event.preventDefault();
+        //         form.appendChild(statusMessage);
+    
+        //         //AJAX
+        //         let request = new XMLHttpRequest();
+        //         request.open("GET", 'https://swapi.dev/api/people/');
+    
+        //         request.setRequestHeader("Content-Type", "application/json");
+    
+        //         // let formData = new FormData(form);
+    
+        //         request.send();
+    
+        //         request.onreadystatechange = function() {
+        //             if (request.readyState < 4) {
+        //                 statusMessage.innerHTML = message.loading;
+    
+        //             } else if (request.readyState === 4) {
+        //                 if (request.status == 200 && request.status < 300) {
+        //                     console.log({res: JSON.parse(request.response)});
+        //                     statusMessage.innerHTML = message.success;
+        //                     // добавіть Kонтент на страницу
+        //                 }
+        //                 else {
+        //                     statusMessage.innerHTML = message.failure;
+        //                     console.log({request});
+        //                 }
+        //             }
+        //         }
+        //         for (let i = 0; i < input.length; i++) {
+        //             input[i].value = '';
+        //             //очищаем поля ввода
+        //         }
+    
+    
+    
+        //     });
 
 
     
